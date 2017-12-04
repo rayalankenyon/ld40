8f84dd303c18947773115d5588675b79bd019150
Rayalan Kenyon <rayalan.kenyon@gmail.com>
2017-12-03 18:42:16 -0600

    some kind of gameplay

--- src/main.c
+++ src/main.c
@@ -21,13 +21,21 @@
 #define SPRITE_SHEET_NAME "sprites.png"
 #define MAP_GRID_SIZE 1024
 #define MAX_ARMY_SIZE 256
-#define GRID_SIZE 16
+#define GRID_SIZE 32
 #define LOG_FILE "log.txt"
 #define SPRITE_SIZE 8
 
 #define UNIT_TILES_PER_SECOND 10.0f
-#define UNIT_ANIMATION_FRAMES 4
+#define UNIT_ANIMATION_FRAMES 8
 #define UNIT_ANIMATION_FRAME_TIME 0.1f
+
+#define START_UNITS 5
+#define START_HP 10
+#define START_RESOURCE 10
+#define UNIT_COST 10
+
+// NOTE(rayalan): 1.0 maybe for a get the highest score you can type of game
+#define RESOURCE_DRAIN_TIME 1.0f
 
 //=============================================================================
 //
@@ -84,22 +92,20 @@ void right_string(float x, float y, float size, char *text) {
 
 typedef enum Cooldown {
     COOLDOWN_ATTACK,
-    COOLDOWN_EAT,
-    COOLDOWN_HEAL,
-    COOLDOWN_PILL,
+    COOLDOWN_HARVEST,
     COOLDOWN_MAX
 } Cooldown;
-
 
 typedef enum Tile_Type {
     TILE_TYPE_WATER,
     TILE_TYPE_GRASS,
+    TILE_TYPE_WALKABLE,
     TILE_TYPE_TREE,
     TILE_TYPE_TREE_RED,
     TILE_TYPE_TREE_ORANGE,
     TILE_TYPE_ROCK,
     TILE_TYPE_SHRUB,
-    TILE_TYPE_SHRUB_PRUPLE,
+    TILE_TYPE_SHRUB_PURPLE,
     TILE_TYPE_MAX
 } Tile_Type;
 
@@ -122,6 +128,7 @@ typedef struct Unit {
     int flags; // is_moving / etc
     int animation_frame;
     float animation_time;
+    float cooldown[COOLDOWN_MAX];
 } Unit;
 
 // 2 bytes * 1024 * 1024 = 2 MB 
@@ -147,6 +154,7 @@ typedef struct Game_State {
     int enemy_count;
     Unit *selection[MAX_ARMY_SIZE];
     int selection_count;
+    float resource_ticks;
 } Game_State;
 
 Sys_Config init(int argc, char **argv) {
@@ -196,13 +204,15 @@ Sys_Config init(int argc, char **argv) {
             // this means that the type can indicate any texture from the bottom row of the sprite sheet
             int k = (int)rand() % 100;
             int t = 0;
-            if(k <= 15) { t = TILE_TYPE_WATER; }
-            else if (k <= 85) { t = TILE_TYPE_GRASS; }
-            else if (k <= 93) { t = TILE_TYPE_TREE; }
+            if (k <= 93) { t = TILE_TYPE_GRASS; }
+            else if (k <= 95) { t = TILE_TYPE_TREE; }
             else if (k <= 96) { t = TILE_TYPE_TREE_RED; }
             else if (k <= 97) { t = TILE_TYPE_TREE_ORANGE; }
+            else if (k <= 98) { t = TILE_TYPE_SHRUB; }
+            else if (k <= 99) { t = TILE_TYPE_SHRUB_PURPLE; }
             else { t = TILE_TYPE_ROCK; }
             state->tile[i][j].type = t;
+            state->tile[i][j].resource = 10 * t;
         }
     }
 
@@ -213,16 +223,17 @@ Sys_Config init(int argc, char **argv) {
     // ========================================================================
     state->ally_count = 0;
     state->enemy_count = 0;
+
     for(int i = state->camera.x; i < state->camera.x + GRID_SIZE; i++) {
         for(int j = state->camera.y; j < state->camera.y + GRID_SIZE; j++) {
             if(state->tile[i][j].type == TILE_TYPE_GRASS) {
                 unsigned int k = rand() % 100;
-                if(k <= 8 && state->ally_count < MAX_ARMY_SIZE) {
-                    state->ally[state->ally_count].x = (float)i + 0.5f;
-                    state->ally[state->ally_count].y = (float)j + 0.5f;
-                    state->ally[state->ally_count].type = k > 4 ? UNIT_TYPE_MALE : UNIT_TYPE_FEMALE;
-                    state->ally[state->ally_count].hp = 100;
-                    state->ally[state->ally_count].resource = 100;
+                if(k <= 2 && state->ally_count < START_UNITS) {
+                    state->ally[state->ally_count].x = (float)i;
+                    state->ally[state->ally_count].y = (float)j;
+                    state->ally[state->ally_count].type = k > 1 ? UNIT_TYPE_MALE : UNIT_TYPE_FEMALE;
+                    state->ally[state->ally_count].hp = START_HP;
+                    state->ally[state->ally_count].resource = START_RESOURCE;
                     state->ally_count++;
                 }    
             }
@@ -245,10 +256,13 @@ inline void init_gl(int w, int h)
 #define color_red 1.0f, 0.0f, 0.0f
 #define color_selection_box 0.2f, 0.7f, 0.2f, 0.4f
 #define color_white 1.0f, 1.0f, 1.0f
+#define color_ally 0.0f, 1.0f, 1.0f
 
 void loop(Sys_State *sys) {
     Game_State *state = (Game_State *)sys->memory.ptr;
     init_gl(sys->width, sys->height); 
+
+    state->resource_ticks += sys->dt;
 
     if(sys_key_pressed(SYS_MOUSE_LEFT)) {
         state->mouse_pressed = vec2(sys->mouse.x, sys->mouse.y);
@@ -257,22 +271,20 @@ void loop(Sys_State *sys) {
         state->mouse_released = vec2(sys->mouse.x, sys->mouse.y);
         // TODO(rayalan): this is where you select units
         //
-        int pressed_x = state->camera.x + (int)(state->mouse_pressed.x * GRID_SIZE / sys->width);
-        int pressed_y = state->camera.y + (int)(state->mouse_pressed.y * GRID_SIZE / sys->height);
-        int released_x = state->camera.x + (int)(sys->mouse.x * GRID_SIZE / sys->width);
-        int released_y = state->camera.y + (int)(sys->mouse.y * GRID_SIZE / sys->height);
-        int box_left = (pressed_x <= released_x) ? pressed_x : released_x;
-        int box_right = (pressed_x >= released_x) ? pressed_x : released_x;
-        int box_top = (pressed_y <= released_y) ? pressed_y : released_y;
-        int box_bottom = (pressed_y >= released_y) ? pressed_y : released_y;
+        float pressed_x = state->camera.x + state->mouse_pressed.x * GRID_SIZE / sys->width;
+        float pressed_y = state->camera.y + state->mouse_pressed.y * GRID_SIZE / sys->height;
+        float released_x = state->camera.x + sys->mouse.x * GRID_SIZE / sys->width;
+        float released_y = state->camera.y +  sys->mouse.y * GRID_SIZE / sys->height;
+        float box_left = (pressed_x <= released_x) ? pressed_x : released_x;
+        float box_right = (pressed_x >= released_x) ? pressed_x : released_x;
+        float box_top = (pressed_y <= released_y) ? pressed_y : released_y;
+        float box_bottom = (pressed_y >= released_y) ? pressed_y : released_y;
 
         state->selection_count = 0;
 
         for(int i = 0; i < MAX_ARMY_SIZE; i++) {
             if(state->ally[i].type > UNIT_TYPE_NONE) {
-                int x = (int)state->ally[i].x;
-                int y = (int)state->ally[i].y;
-                if(x <= box_right && x >= box_left && y <= box_bottom && y >= box_top) {
+                if(state->ally[i].x <= box_right && state->ally[i].x >= box_left && state->ally[i].y <= box_bottom && state->ally[i].y >= box_top) {
                     state->selection[state->selection_count] = &state->ally[i];
                     state->selection_count++;
                 }
@@ -283,8 +295,8 @@ void loop(Sys_State *sys) {
     if(sys_key_pressed(SYS_MOUSE_RIGHT)) {
         if(state->selection_count) {
             for(int i = 0; i < state->selection_count; i++) {
-                state->selection[i]->look_x = (float)state->camera.x + ((float)sys->mouse.x * GRID_SIZE / sys->width);
-                state->selection[i]->look_y =  (float)state->camera.y + ((float)sys->mouse.y * GRID_SIZE / sys->height);
+                state->selection[i]->look_x = state->camera.x + ((int)sys->mouse.x * GRID_SIZE / sys->width);
+                state->selection[i]->look_y =  state->camera.y +((int)sys->mouse.y * GRID_SIZE / sys->height);
                 state->selection[i]->flags |= UNIT_FLAG_MOVING; 
                 state->selection[i]->animation_time = (float)sys_time_now();
             }
@@ -328,43 +340,95 @@ void loop(Sys_State *sys) {
     if(sys_key_pressed(SYS_KEY_ESC)) { // select none
         state->selection_count = 0;
     }
+    if(sys_key_pressed('S')) { // stop 
+        for(int i = 0; i < state->selection_count; i++) {
+            state->selection[i]->look_x = state->selection[i]->x;
+            state->selection[i]->look_y = state->selection[i]->y;
+        }
+    }
+    if(sys_key_pressed('D')) { // disperse
+        for(int i = 0; i < state->selection_count; i++) {
+            state->selection[i]->look_x = rand() % GRID_SIZE + state->camera.x;
+            state->selection[i]->look_y = rand() % GRID_SIZE + state->camera.y;
+            state->selection[i]->flags |= UNIT_FLAG_MOVING;
+        }
+    }
+
+    if(sys_key_pressed('E')) { // HARVEST / EAT
+        for(int i = 0; i < state->selection_count; i++) {
+            int tx = (int)state->selection[i]->x+1;
+            int ty = (int)state->selection[i]->y+1;
+            if(state->tile[tx][ty].type > TILE_TYPE_GRASS && state->tile[tx][ty].resource > 0) {
+                state->tile[tx][ty].resource--;
+                state->selection[i]->resource++;
+                if(state->tile[tx][ty].resource == 0) {
+                    state->tile[tx][ty].type = TILE_TYPE_GRASS;
+                }
+            }
+        }
+    }
+    if(sys_key_pressed('Q')) {
+        for(int i = 0; i < state->selection_count; i++) {
+            if(state->selection[i]->resource >= UNIT_COST) {
+                state->selection[i]->resource -= UNIT_COST;
+                state->ally[state->ally_count].type = state->selection[i]->type;
+                state->ally[state->ally_count].x = state->selection[i]->x;
+                state->ally[state->ally_count].y = state->selection[i]->y;
+                state->ally[state->ally_count].look_x = state->selection[i]->look_x;
+                state->ally[state->ally_count].look_y = state->selection[i]->look_y;
+                state->ally[state->ally_count].hp = START_HP;
+                state->ally[state->ally_count].resource = 0;
+                state->ally_count++;
+            }
+        }
+    }
 
     // UPDATE allied units
     for(int i = 0; i < MAX_ARMY_SIZE; i++) {
         
+        if(state->resource_ticks >= RESOURCE_DRAIN_TIME && state->ally[i].resource > 0) {
+            state->ally[i].resource -= 1;
+        }
         if(state->ally[i].x < state->ally[i].look_x + 0.1f && state->ally[i].x > state->ally[i].look_x - 0.1f
                 && (state->ally[i].flags & UNIT_FLAG_MOVING) && state->ally[i].y < state->ally[i].look_y + 0.1f 
                 && state->ally[i].y > state->ally[i].look_y - 0.1f) {
             state->ally[i].flags ^= UNIT_FLAG_MOVING;
+            state->ally[i].x = state->ally[i].look_x;
+            state->ally[i].y = state->ally[i].look_y;
             state->ally[i].animation_frame = 0;
             state->ally[i].animation_time = 0;
         }
 
         if(state->ally[i].flags & UNIT_FLAG_MOVING) {
+
             state->ally[i].animation_time += sys->dt;
             if(state->ally[i].animation_time >= UNIT_ANIMATION_FRAME_TIME) {
                 state->ally[i].animation_frame = (state->ally[i].animation_frame + 1) % UNIT_ANIMATION_FRAMES;
                 state->ally[i].animation_time = 0;
             }
-            if(state->ally[i].x < state->ally[i].look_x) {
-                state->ally[i].x += sys->dt * UNIT_TILES_PER_SECOND;
-            }
-            if(state->ally[i].y < state->ally[i].look_y) {
-                state->ally[i].y += sys->dt * UNIT_TILES_PER_SECOND;
-            }
-            if(state->ally[i].x > state->ally[i].look_x) {
-                state->ally[i].x -= sys->dt * UNIT_TILES_PER_SECOND; 
-            }
-            if(state->ally[i].y > state->ally[i].look_y) {
-                state->ally[i].y -= sys->dt * UNIT_TILES_PER_SECOND;
-            }
-        }
 
+            float dx = 0.0f;
+            float dy = 0.0f;
+
+            if(state->ally[i].x < state->ally[i].look_x - 0.1f) {
+                dx = sys->dt * UNIT_TILES_PER_SECOND;
+            } else if(state->ally[i].x > state->ally[i].look_x + 0.1f) {
+                dx = -1.0f * sys->dt * UNIT_TILES_PER_SECOND; 
+            }
+
+            if(state->ally[i].y < state->ally[i].look_y - 0.1f) {
+                dy = sys->dt * UNIT_TILES_PER_SECOND;
+            } else if(state->ally[i].y > state->ally[i].look_y + 0.1f) {
+               dy = -1.0f * sys->dt * UNIT_TILES_PER_SECOND;
+            }
+
+            state->ally[i].x += dx;
+            state->ally[i].y += dy;
+        }
     }
 
-
-
-
+    // UPDATE tick timers
+    if(state->resource_ticks >= RESOURCE_DRAIN_TIME) { state->resource_ticks = 0.0f; }
 
     // DRAW map
     float render_size = 1.0f/ GRID_SIZE;
@@ -412,15 +476,14 @@ void loop(Sys_State *sys) {
             for(int i = 0; i < state->ally_count; i++) {
                 int map_x = (int)state->ally[i].x;
                 int map_y = (int)state->ally[i].y;
-                
+
                 if(map_x >= state->camera.x && map_x <= state->camera.x + GRID_SIZE
                    && map_y >= state->camera.y && map_y <= state->camera.y + GRID_SIZE) {
-
-                    int draw_x = map_x - state->camera.x;
-                    int draw_y = map_y - state->camera.y;
+                    
+                    float draw_x = state->ally[i].x - state->camera.x;
+                    float  draw_y = state->ally[i].y - state->camera.y;
                     int tx = state->ally[i].animation_frame;
                     int ty = state->sprite_sheet.width / SPRITE_SIZE - state->ally[i].type + 1;
-
                     glTexCoord2f(tx * tile_size, (ty-1) * tile_size);
                     glVertex2f(draw_x * render_size, (draw_y+1) * render_size);
                     glTexCoord2f((tx+1) * tile_size, (ty-1) * tile_size);
@@ -429,7 +492,6 @@ void loop(Sys_State *sys) {
                     glVertex2f((draw_x+1) * render_size, draw_y * render_size);
                     glTexCoord2f(tx * tile_size, ty * tile_size);
                     glVertex2f(draw_x * render_size, draw_y * render_size);
-                    //__debugbreak();
                 }
             }
         glEnd();
@@ -451,47 +513,23 @@ void loop(Sys_State *sys) {
         glEnd();
     }
 
-    
-    // DRAW play area text here
 
-#if 0 // NOTE(rayalan): drawing long string is super expensive
+    // draw total hp / resource counts here
+    uint64_t total_hp = 0;
+    uint64_t total_resource = 0;
+
+    for(int i = 0; i < state->ally_count; i++) {
+        total_hp += state->ally[i].hp;
+        total_resource += state->ally[i].resource;
+    }
+
+    char hp_buf[64], res_buf[64];
+    sprintf(hp_buf, "%lld", total_hp);
+    sprintf(res_buf, "%lld", total_resource);
+
     glColor3f(color_white);
-    left_string(50, 50, 2.0f, "this string is on the left");
-
-    centered_string(sys->width/2.0f, sys->height/2.0f, 2.0f, "hahahah");
-    right_string(sys->width - 50, sys->height * 0.1f, 2.0f, "resources");
-    centered_string(sys->width/2.0f, sys->height * 0.7f, 1.0f, "you must construct additional pylons");
-#endif
-    
-    // draw minimap
-    //glColor3f(color_pink);
-#if 0 
-    glBindTexture(GL_TEXTURE_2D, state->sprite_sheet.id);
-    glColor3f(1.0f, 1.0f, 1.0f);
-    glPushMatrix();
-        glLoadIdentity();
-        glOrtho(0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f);
-        glBegin(GL_QUADS);
-            glTexCoord2f(0.0f, 0.0f);
-            glVertex2f(0.0f, 1.0f);
-            glTexCoord2f(0.0625f, 0.0f);
-            glVertex2f(1.0f, 1.0f);
-            glTexCoord2f(0.0625f, 0.0625f);
-            glVertex2f(1.0f, 0.8f);
-            glTexCoord2i(0.0f, 0.0625f);
-            glVertex2f(0.0f, 0.8f);
-        glEnd();
-    glPopMatrix();
-    glBindTexture(GL_TEXTURE_2D, 0);
-#endif 
-
-#if 1 // draw the number of units selected
-    char sel_text_buffer[64];
-    sprintf(sel_text_buffer, "%i units selected", state->selection_count);
-    glColor3f(color_white);
-    centered_string(sys->width/2.0f, sys->height * 0.1f, 1.0f, sel_text_buffer);
-#endif 
-
+    right_string((float)sys->width - 32.0f, sys->height * 0.02f, 2.0f, hp_buf);
+    right_string((float)sys->width - 32.0f, sys->height * 0.05f, 2.0f, res_buf);
 }
 
 
